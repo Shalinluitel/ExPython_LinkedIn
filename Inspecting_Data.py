1355a4838a997b34a7191cdada9ae6978b86425b
Shalin Luitel <shalin2346@gmail.com>
2022-03-01 23:28:16 -0500

    py

--- Inspecting_Data.py
+++ Inspecting_Data.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from openpyxl.workbook import Workbook
 
-df_excel = pd.read_excel("regions.xlsx")
+
 df_csv = pd.read_csv("Names.csv", header=None)
-df_txt = pd.read_csv("data.txt", delimiter="\t")
-
-
 df_csv.columns = ["First", "Last", "Address", "City", "State", "Area Code", "AC2"]
 
-print(df_csv)
+#print(df_csv[["Last","State"]])
 
-df_csv.to_excel("modified.xlsx")
+print(df_csv["First"][0:3])
 
 
+
+ 
